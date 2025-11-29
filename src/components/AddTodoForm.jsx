32cb6622d8c1f83a9597d10aa4ab06a24429c4a8
Cie <jocelynbendoy26@gmail.com>
2025-11-29 09:47:21 +0800

    Implement AddTodoForm component

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -2,3 +2,41 @@
 
 
 
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todos/todosSlice";
+
+function AddTodoForm({ theme }) {
+  const [title, setTitle] = useState("");
+  const dispatch = useDispatch();
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const todoText = title.trim();
+    
+    if (todoText === "") {
+      alert("You must write something!");
+      return;
+    }
+    
+    dispatch(addTodo(todoText));
+    setTitle("");
+  };
+
+  return (
+    <form onSubmit={handleAdd}>
+      <input
+        className={`todo-input ${theme}-input`}
+        type="text"
+        placeholder="Add a task."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button className={`todo-btn ${theme}-button`} type="submit">
+        I Got This!
+      </button>
+    </form>
+  );
+}
+
+export default AddTodoForm;
